238567ca72b65f2aa060152b9b6d3372a7864b50
vladshein <vlshein1@gmail.com>
2025-11-14 08:34:13 +0200

    fixed console.log and modified if statements

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,26 +3,23 @@ import HttpError from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res) => {
     const contacts = await contactsService.listContacts();
-    console.log(contacts);
     res.json(contacts);
 };
 
 export const getOneContact = async (req, res) => {
     const contact = await contactsService.getContactById(req.params.id);
-    if (contact) {
-        res.json(contact);
-    } else {
+    if (!contact) {
         throw HttpError(404, "Not found");
     }
+    res.json(contact);
 };
 
 export const deleteContact = async (req, res) => {
     const contact = await contactsService.removeContact(req.params.id);
-    if (contact) {
-        res.json(contact);
-    } else {
+    if (!contact) {
         throw HttpError(404, "Not found");
     }
+    res.json(contact);
 };
 
 export const createContact = async (req, res) => {
@@ -35,9 +32,8 @@ export const updateContact = async (req, res) => {
     const { name, email, phone } = req.body;
 
     const contact = await contactsService.updateContact(req.params.id, name, email, phone);
-    if (contact) {
-        res.status(200).json(contact);
-    } else {
+    if (!contact) {
         throw HttpError(404, `Not found`);
     }
+    res.status(200).json(contact);
 };
