ae983818e4d2862449347e21d26fadd045ee66ea
vladshein <vlshein1@gmail.com>
2025-11-15 14:17:04 +0200

    added env variables to sequelize

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,7 +1,15 @@
 import { Sequelize } from "sequelize";
 
-import { databaseConfig } from "../config.js";
-
-const sequelize = new Sequelize(databaseConfig);
+const sequelize = new Sequelize({
+    dialect: process.env.DATABASE_DIALECT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
+    dialectOptions: {
+        ssl: true,
+    },
+});
 
 export default sequelize;
